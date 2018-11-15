01fcc524fc8317e7d5541035252f6e6cf1f721a6
Angel Paunchev <apaunchev@gmail.com>
2018-11-15 21:34:51 +0200

    Updates

---
+++ precache-manifest.e9c5779b43894f0d140d3ccf92d0ca23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dd7b031410f20fbbba4f",
+    "url": "/static/js/main.dd7b0314.chunk.js"
+  },
+  {
+    "revision": "2c80978b796b958a3956",
+    "url": "/static/js/1.2c80978b.chunk.js"
+  },
+  {
+    "revision": "dd7b031410f20fbbba4f",
+    "url": "/static/css/main.8410063b.chunk.css"
+  },
+  {
+    "revision": "2c80978b796b958a3956",
+    "url": "/static/css/1.c562b618.chunk.css"
+  },
+  {
+    "revision": "c0a9f5ef7293fa42001e6f838a1b5fe7",
+    "url": "/index.html"
+  }
+];

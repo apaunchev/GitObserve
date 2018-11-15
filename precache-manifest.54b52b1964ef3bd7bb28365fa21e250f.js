4eb9f54b7027680cbcba88641a734f355a77700a
Angel Paunchev <apaunchev@gmail.com>
2018-11-15 21:54:59 +0200

    Updates

---
+++ precache-manifest.54b52b1964ef3bd7bb28365fa21e250f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f708cb8ddcaa44044f4",
+    "url": "/gitobserve/static/js/runtime~main.9f708cb8.js"
+  },
+  {
+    "revision": "dd7b031410f20fbbba4f",
+    "url": "/gitobserve/static/js/main.dd7b0314.chunk.js"
+  },
+  {
+    "revision": "2c80978b796b958a3956",
+    "url": "/gitobserve/static/js/1.2c80978b.chunk.js"
+  },
+  {
+    "revision": "dd7b031410f20fbbba4f",
+    "url": "/gitobserve/static/css/main.8410063b.chunk.css"
+  },
+  {
+    "revision": "2c80978b796b958a3956",
+    "url": "/gitobserve/static/css/1.c562b618.chunk.css"
+  },
+  {
+    "revision": "3cc581140143a890caceecd6287d23bd",
+    "url": "/gitobserve/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e9c5779b43894f0d140d3ccf92d0ca23.js"
+  "/gitobserve/precache-manifest.54b52b1964ef3bd7bb28365fa21e250f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/gitobserve/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

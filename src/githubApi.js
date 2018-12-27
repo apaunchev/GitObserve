e703bef3bef4ffea5544076b00a1ef3c86ca97cf
Angel Paunchev <apaunchev@gmail.com>
2018-12-27 15:20:06 +0200

    remove affiliations

--- src/githubApi.js
+++ src/githubApi.js
@@ -34,7 +34,6 @@ export const queries = {
         viewer {
           watching(
             first: 100
-            affiliations: [OWNER, COLLABORATOR, ORGANIZATION_MEMBER]
             ${afterParam}
           ) {
             edges {

293a749deac11be9799fa86315f8f3afabf86f87
Angel Paunchev <apaunchev@gmail.com>
2019-01-04 09:53:58 +0200

    add prop-types

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { Provider } from "react-redux";
 import { HashRouter as Router, Route } from "react-router-dom";
 import App from "./App";
@@ -10,5 +11,9 @@ const Root = ({ store }) => (
     </Router>
   </Provider>
 );
+
+Root.propTypes = {
+  store: PropTypes.object.isRequired
+};
 
 export default Root;

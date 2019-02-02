1772dd45ceac8c18f9cb5515237efad4232d9659
Angel Paunchev <apaunchev@gmail.com>
2019-02-02 12:23:44 +0200

    remove API orderBy, done on the frontend

--- src/githubApi.js
+++ src/githubApi.js
@@ -65,7 +65,6 @@ export const queries = {
           pullRequests(
             last: 50
             states: [OPEN]
-            orderBy: { field: CREATED_AT, direction: DESC }
           ) {
             edges {
               node {

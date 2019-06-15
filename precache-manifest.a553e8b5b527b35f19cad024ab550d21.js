fb1eafee8fefeb243f768b082eb65c67c34448e5
Angel Paunchev <apaunchev@gmail.com>
2019-06-15 12:16:01 +0300

    Updates

--- precache-manifest.a553e8b5b527b35f19cad024ab550d21.js
+++ precache-manifest.a553e8b5b527b35f19cad024ab550d21.js
@@ -4,8 +4,8 @@ self.__precacheManifest = [
     "url": "/GitObserve/static/js/runtime~main.86b8849a.js"
   },
   {
-    "revision": "e15e031415b8a9f2a131",
-    "url": "/GitObserve/static/js/main.20633b27.chunk.js"
+    "revision": "f1ca5d97c6f342fcd680",
+    "url": "/GitObserve/static/js/main.edde4054.chunk.js"
   },
   {
     "revision": "da63b25b4529db79f26d",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/GitObserve/static/css/2.a38bb906.chunk.css"
   },
   {
-    "revision": "2aa9963d0eafddcaa01b9e16e1ee6575",
+    "revision": "0669845f54f33cd4c7bc22548b7bb251",
     "url": "/GitObserve/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/GitObserve/precache-manifest.b07fdb4c9daded95dc9e13bb6e944053.js"
+  "/GitObserve/precache-manifest.a553e8b5b527b35f19cad024ab550d21.js"
 );
 
 workbox.clientsClaim();

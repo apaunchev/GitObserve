2c4aa3974376e775dbfd15c52d5fd43b49c1f8f6
Angel Paunchev <apaunchev@gmail.com>
2020-06-21 12:24:48 +0300

    Updates

---
+++ precache-manifest.1a3d10604a48eae3cb834280910a4679.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03bf737baba1d4ce5cc60b80c91b6dda",
+    "url": "/GitObserve/index.html"
+  },
+  {
+    "revision": "221a327e6dc118e3823e",
+    "url": "/GitObserve/static/css/2.0a8c2628.chunk.css"
+  },
+  {
+    "revision": "221a327e6dc118e3823e",
+    "url": "/GitObserve/static/js/2.bd64f2cd.chunk.js"
+  },
+  {
+    "revision": "f576a62dc8a23071157d6bb7dca7fa31",
+    "url": "/GitObserve/static/js/2.bd64f2cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fea88027c7e960667f5",
+    "url": "/GitObserve/static/js/main.1f66fa90.chunk.js"
+  },
+  {
+    "revision": "0597ce0fd31f373379a7",
+    "url": "/GitObserve/static/js/runtime-main.a28af8f4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/GitObserve/precache-manifest.b5cd66088a2fb5eb097509a8c80dff16.js"
+  "/GitObserve/precache-manifest.1a3d10604a48eae3cb834280910a4679.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/GitObserve/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/GitObserve/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
